82aeebe6f1f3445de9074b435c8a6dfc177d4613
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-05-17 20:02:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 print("Welcome to the Sorting Hat! I, the sorting hat, will ask you a few questions that you need to answer truthfully to be put into your house.")
 points = 0
 
+def answer() : 
+  choice = input()
+  if choice == "I":
+    points += 1
+  elif choice == "II":
+    points += 2
+  elif choice == "III":
+    points += 3
+  elif choice == "IV":
+    points += 4
+
 print("Enter your answers in this format: 'I', 'II', 'III', 'IV'")
 
 print("Question 1: World War III has broken out and you have joined your countries military? What do you do?")
@@ -9,16 +20,7 @@ print("II: Be extremely helpful to all your fellow soldiers.")
 print("III: Share an excellent battle plan with your general that may lead your side to victory.")
 print("IV: Be extremely helpful to your general in hope of ranking up.")
 
-choice0 = input()
-if choice0 == "I":
-  points += 1
-elif choice0 == "II":
-  points += 2
-elif choice0 == "III":
-  points += 3
-elif choice0 == "IV":
-  points += 4
-
+answer()
 
 
 
@@ -28,15 +30,7 @@ print("II: Help rebuild it.")
 print("III: Let it be, but keep some troops to guard it.")
 print("IV: Burn it to the ground.")
 
-choice1 = input()
-if choice1 == "I":
-  points += 1
-elif choice1 == "II":
-  points += 2
-elif choice1 == "III":
-  points += 3
-elif choice1 == "IV":
-  points += 4
+answer()
 
 
 
@@ -48,15 +42,7 @@ print("II: Wait for renforcements.")
 print("III: Surround it and then wait for renforcements.")
 print("IV: Surround it, slowly advance, and take it over. If they show signs of resistance burn it to the ground.")
 
-choice2 = input()
-if choice2 == "I":
-  points += 1
-elif choice2 == "II":
-  points += 2
-elif choice2 == "III":
-  points += 3
-elif choice2 == "IV":
-  points += 4
+answer()
 
 
 
@@ -67,15 +53,7 @@ print("I: Keep all of it.")
 print("II: Spend all of it on rebuilding.")
 print("III: Keep 25 billion and spend 25 billion on rebuilding.")
 print("IV: Spend as much as is needed on rebuilding and keep the rest.")
-choice3 = input()
-if choice3 == "I":
-  points += 1
-elif choice3 == "II":
-  points += 2
-elif choice3 == "III":
-  points += 3
-elif choice3 == "IV":
-  points += 4
+answer()
 
   
   
